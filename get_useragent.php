2eccca4016c0856d0e20cb2e60dada573a757a51
Hello World! <67582014+arspujalabs@users.noreply.github.com>
2022-02-10 23:09:40 +0700

    Create get_useragent.php

---
+++ get_useragent.php
@@ -0,0 +1,9 @@
+<?php
+// fetch user agent user
+$uagen = $_SERVER['HTTP_USER_AGENT'];
+// display results
+echo $uagen;
+// e.q
+// Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.94 Safari/537.36
+
+?>
